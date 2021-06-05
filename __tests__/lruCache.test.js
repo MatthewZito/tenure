9762c22bce7cbfac65ce91a62b9813683e5e31f9
goldmund <matthewtzito@gmail.com>
2021-06-04 22:24:24 -0700

    chore: align cache to dll

--- __tests__/lruCache.test.js
+++ __tests__/lruCache.test.js
@@ -5,103 +5,141 @@ import LruCache from '../lib';
 const subject = 'the lru cache';
 const init = cap => new LruCache(cap);
 
-// test(`${subject} pops off entries if it meets or exceeds the capacity`, t => {
-// 	const cap = 6;
+test(`${subject} pops off entries if it meets or exceeds the capacity`, t => {
+	const cap = 6;
 
-// 	const lru = init(cap);
+	const lru = init(cap);
 
-// 	for (let i = 0; i <= cap; i++) {
-// 		lru.put(`key${i}`, 6);
-// 	}
+	for (let i = 0; i <= cap; i++) {
+		lru.put(`key${i}`, 6);
+	}
 
-// 	lru.put('END', 6);
-// 	lru.put('END', 6);
-// 	lru.put('END', 6);
+	lru.put('END', 6);
+	lru.put('END', 6);
+	lru.put('END', 6);
 
-// 	t.equals(lru.adapter.length, cap);
-// 	t.equals(lru.adapter.head.data.key, 'END');
-// 	t.equals(lru.cache.size, cap);
-// 	t.equals(lru.get('key1'), null);
-// 	t.end();
-// });
+	t.equals(lru.adapter.length, cap);
+	t.equals(lru.adapter.head.data.key, 'END');
+	t.equals(lru.cache.size, cap);
+	t.equals(lru.get('key1'), null);
+	t.end();
+});
 
 
-// test(`${subject} moves the most recently retrieved value to the head of its internal list`, t => {
-// 	const cap = 6;
+test(`${subject} moves the most recently retrieved value to the head of its internal list`, t => {
+	const cap = 6;
 
-// 	const lru = init(cap);
+	const lru = init(cap);
 
-// 	for (let i = 0; i <= cap; i++) {
-// 		lru.put(`key${i}`, i);
-// 	}
+	let i = 0;
+	while (i <= cap) {
+		lru.put(`key${i}`, i);
+		lru.get(`key${i}`);
+		i++;
+	}
 
-// 	const recent = lru.get('key3');
+	t.equals(lru.adapter.head.data.key, `key${i - 1}`);
+	t.equals(lru.adapter.head.data.value, i - 1);
 
-// 	t.equals(lru.adapter.head.data.key, 'key3');
-// 	t.equals(lru.adapter.head.data.value, recent);
-// 	t.end();
-// });
+	const recent = lru.get('key3');
 
-// test(`${subject} returns null when retrieving a non-extant value`, t => {
-// 	const lru = init();
+	t.equals(lru.adapter.head.data.key, 'key3');
+	t.equals(lru.adapter.head.data.value, recent);
+	t.end();
+});
 
-// 	t.equals(lru.get('enoexist'), null);
-// 	t.end();
-// });
+test(`${subject} returns null when retrieving a non-extant value`, t => {
+	const lru = init();
 
-// test(`${subject} returns null when deleting a non-extant value`, t => {
-// 	const lru = init();
+	t.equals(lru.get('enoexist'), null);
+	t.end();
+});
 
-// 	t.equals(lru.del('enoexist'), null);
-// 	t.end();
-// });
+test(`${subject} returns null when deleting a non-extant value`, t => {
+	const lru = init();
 
-// test(`${subject} size remains consistent with that of the internal list`, t => {
-// 	const cap = 20;
+	t.equals(lru.del('enoexist'), null);
+	t.end();
+});
 
-// 	const lru = init(cap);
+test(`${subject} size remains consistent with that of the internal list`, t => {
+	const cap = 20;
 
-// 	for (let i = 0; i <= cap; i++) {
-// 		lru.put(`key${i}`, i);
-// 	}
+	const lru = init(cap);
 
-// 	lru.get('key1');
-// 	lru.get('key17');
-// 	lru.del('key1');
+	for (let i = 0; i <= cap; i++) {
+		lru.put(`key${i}`, i);
+		lru.get('key1');
+	}
 
-// 	for (let j = 0; j <= 9; j++) {
-// 		lru.put(`key${j}`, j);
-// 	}
+	lru.get('key1');
+	lru.get('key17');
+	lru.del('key1');
 
-// 	lru.get('key9');
-// 	lru.del('key7');
+	for (let j = 0; j <= 9; j++) {
+		lru.put(`key${j}`, j);
+	}
 
-// 	t.equals(lru.cache.size, lru.adapter.size());
-// 	t.end();
-// });
+	lru.get('key9');
+	lru.del('key7');
 
-// test to break
-test(`${subject} maintains integrity after a large amount of transactions`, t => {
+	t.equals(lru.cache.size, lru.adapter.size());
+	t.end();
+});
+
+test(`${subject} size remains consistent with that of the internal list`, t => {
+	const cap = 20;
+
+	const lru = init(cap);
+
+	for (let i = 0; i <= 50; i++) {
+		lru.put(`key${i}`, i);
+	}
+
+	lru.get('key49');
+
+	t.equals(lru.cache.size, lru.adapter.size());
+	t.end();
+});
+
+test(`${subject} internal list maintains integrity when compared to manually observed iterations`, t => {
 	const cap = 3;
 
 	const lru = init(cap);
 
-	//
-	for (let i = 1; i <= 5; i++) {
-		lru.put(`key${i}`, i);
-		// console.log(`pushed ${i} onto head\nNew head is ${lru.adapter.head.data.key}\nNew tail is ${lru.adapter.tail.data.key}`)
-		// console.log(`\nll is`, lru.adapter)
-		if (i > 1) {
-			lru.get('key1');
-			// console.log(`pushed key1 onto head\nNew head is ${lru.adapter.head.data.key}\nNew tail is ${lru.adapter.tail.data.key}`)
-			// console.log(`\nll is`, lru.adapter)
-		}
-		// console.log({ TAIL: lru.adapter.tail });
-	}
+	lru.put('key1'); // head = tail = key1
+	t.equals(lru.adapter.head, lru.adapter.tail);
+	t.equals(lru.adapter.head.data.key, 'key1');
 
-	// console.log(lru.adapter)
-	// t.equals(lru.cache.size, cap);
-	// t.equals(lru.cache.size, lru.adapter.size());
-	t.equals(1,1)
+	lru.put('key2'); // head = k2, tail = k1
+	t.equals(lru.adapter.head.data.key, 'key2');
+	t.equals(lru.adapter.tail.data.key, 'key1');
+	t.equals(lru.adapter.size(), 2);
+
+	lru.get('key1'); // head = k1, tail = k2
+	t.equals(lru.adapter.head.data.key, 'key1');
+	t.equals(lru.adapter.tail.data.key, 'key2');
+	t.equals(lru.adapter.size(), 2);
+
+	lru.put('key3'); // head = k3, tail = k2  | k3, k1, k2 |
+	t.equals(lru.adapter.head.data.key, 'key3');
+	t.equals(lru.adapter.tail.data.key, 'key2');
+	t.equals(lru.adapter.size(), 3);
+
+	lru.get('key1'); // | k1, k3, k2 |
+	t.equals(lru.adapter.head.data.key, 'key1');
+	t.equals(lru.adapter.tail.data.key, 'key2');
+	t.equals(lru.adapter.size(), 3);
+
+	lru.put('key4'); // head = k4, tail = k1 -> | k4, k1, k3 |
+	t.equals(lru.adapter.head.data.key, 'key4');
+	t.equals(lru.adapter.tail.data.key, 'key3');
+
+	lru.get('key1'); // head = k1, tail = k3 -> | k1, k4, k3 |
+	t.equals(lru.adapter.head.data.key, 'key1');
+	t.equals(lru.adapter.tail.data.key, 'key3');
+	t.equals(lru.adapter.size(), 3);
+
+	t.equals(lru.cache.size, lru.adapter.size());
 	t.end();
 });
