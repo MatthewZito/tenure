083697a645ca602b5a1bccb59b5e9afcee8d8987
goldmund <matthewtzito@gmail.com>
2021-06-12 09:35:44 -0700

    feat: add new api methods
    - keys
    - has
    - lru
    - drop
    - resize
    - size
    - capacity
    - eviction callback

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
+[![Build
+Status](https://travis-ci.com/MatthewZito/tenure.svg?branch=master)](https://travis-ci.com/MatthewZito/tenure)
+[![npm version](https://badge.fury.io/js/tenure.svg)](https://badge.fury.io/js/tenure)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 # Tenure | Manageable LRU caching
 
-`Tenure` is a manageable LRU cache instance that uses hashmap lookups and an Open Doubly Linked List to enact the [Least-Recently Used algorithm](https://en.wikipedia.org/wiki/Cache_replacement_policies#Least_recently_used_(LRU))
+`Tenure` is a manageable LRU cache instance that uses hashmap lookups and an Open Doubly Linked List to enact the
+[Least-Recently Used algorithm](https://en.wikipedia.org/wiki/Cache_replacement_policies#Least_recently_used_(LRU))
 
 ## Installation
 
@@ -23,78 +29,136 @@ yarn add tenure
 Commonjs:
 
 ```js
-const LruCache = require('tenure');
+var LruCache = require('tenure');
+
+var cache = new LruCache(100, cb);
 ```
 
 ESM:
 
 ```js
 import LruCache from 'tenure';
+
+const cache = new LruCache(100, cb);
 ```
 
-## API
+## API Reference
 
-### new LruCache(capacity?: Number): LruCache
 
-*Initialize a new cache instance with a given capacity*
+<br><a name="LruCache"></a>
 
-When capacity is reached, the algorithm will begin removing the least-recently used record on every `put` transaction.
-Capacity indicates the maximum number of records to maintain for a given cache.
+# LruCache
 
-Cache defaults to 10.
+* [LruCache](#LruCache)
+    * [new LruCache(capacity, cb)](#new_LruCache_new)
+    * [.get(key)](#LruCache+get) ⇒ <code>any</code> \| <code>null</code>
+    * [.put(key, value)](#LruCache+put) ⇒ <code>boolean</code>
+    * [.del(key)](#LruCache+del) ⇒ <code>boolean</code>
+    * [.keys()](#LruCache+keys) ⇒
+    * [.has(key)](#LruCache+has) ⇒
+    * [.lru()](#LruCache+lru) ⇒ <code>array</code> \| <code>null</code>
+    * [.drop()](#LruCache+drop)
+    * [.resize(cap)](#LruCache+resize) ⇒ <code>number</code>
+    * [.size()](#LruCache+size) ⇒ <code>number</code>
+    * [.capacity()](#LruCache+capacity) ⇒ <code>number</code>
 
-**Example**
 
-```js
-import LruCache from 'tenure';
+<br><a name="new_LruCache_new"></a>
 
-const cache = new LruCache(10);
-```
+## new LruCache(capacity, cb)
+> Implements a canonical Least Recently-Used Cache
 
-### LruCache.get(key: any): (any|null)
 
-*Retrieve an item from the cache; the item will be designated 'most-recently used'*
+| Param | Type | Description |
+| --- | --- | --- |
+| capacity | <code>number</code> | The maximum capacity (items) of the cache; beyond this threshold, the eviction policy is enacted. Defaults to 10 |
+| cb | <code>function</code> | Optional callback to be invoked upon each eviction; called with evicted item key, value |
 
-**Example**
 
-```js
-import LruCache from 'tenure';
+<br><a name="LruCache+get"></a>
 
-const cache = new LruCache(10);
+## lruCache.get(key) ⇒ <code>any</code> \| <code>null</code>
+Retrieve an item from the cache; if extant, the item will be designated 'most-recently used'
+**Returns**: <code>any</code> \| <code>null</code> - The retrieved value, if extant; else, null  
 
-cache.put('key1', 'val1');
-cache.get('key1'); // 'val1'
-cache.get('key4'); // null
-```
+| Param | Type |
+| --- | --- |
+| key | <code>any</code> | 
 
-### LruCache.put(key: any, value: any): any
 
-*Insert or update an item in the cache*
+<br><a name="LruCache+put"></a>
 
-If the list has reached the specified capacity, the least-recently used value is evicted from the cache.
+## lruCache.put(key, value) ⇒ <code>boolean</code>
+Add or update a given key / value pair in the cache
 
-**Example**
+Put transactions will move the key to the head of the cache, designating it as 'most recently-used'
 
-```js
-import LruCache from 'tenure';
+If the cache has reached the specified capacity, Put transactions will also enact the eviction policy,
+thereby removing the least recently-used item
+**Returns**: <code>boolean</code> - A boolean indicating whether an eviction occurred  
 
-const cache = new LruCache(10);
+| Param | Type |
+| --- | --- |
+| key | <code>any</code> | 
+| value | <code>any</code> | 
 
-cache.put('key1', 'val1');
-```
 
-### LruCache.del(key: any): (any|null)
+<br><a name="LruCache+del"></a>
 
-*Remove an item from the cache, if extant*
+## lruCache.del(key) ⇒ <code>boolean</code>
+Remove an item corresponding to a given key from the cache, if extant
+**Returns**: <code>boolean</code> - A boolean indicating whether of not the delete transaction occurred  
 
-**Example**
+| Param | Type |
+| --- | --- |
+| key | <code>any</code> | 
 
-```js
-import LruCache from 'tenure';
 
-const cache = new LruCache(10);
+<br><a name="LruCache+keys"></a>
 
-cache.put('key1', 'val1');
-cache.del('key1'); // 'val1'
-cache.del('key9'); // null
-```
+## lruCache.keys() ⇒
+**Returns**: An array of all keys currently extant in the cache  
+
+<br><a name="LruCache+has"></a>
+
+## lruCache.has(key) ⇒
+Verify the existence of a key in the cache without enacting the eviction policy
+**Returns**: A boolean flag verifying the existence (or lack thereof) of a given key in the cache  
+
+| Param | Type |
+| --- | --- |
+| key | <code>any</code> | 
+
+
+<br><a name="LruCache+lru"></a>
+
+## lruCache.lru() ⇒ <code>array</code> \| <code>null</code>
+**Returns**: <code>array</code> \| <code>null</code> - the least recently-used key / value pair, or null if not extant  
+
+<br><a name="LruCache+drop"></a>
+
+## lruCache.drop()
+Drop all items from the cache, effectively purging it
+
+<br><a name="LruCache+resize"></a>
+
+## lruCache.resize(cap) ⇒ <code>number</code>
+Resizes the cache capacity.
+
+Invoking this transaction will evict all least recently-used items to adjust the cache, where necessary
+**Returns**: <code>number</code> - the number of evictions enacted  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| cap | <code>number</code> | new capacity |
+
+
+<br><a name="LruCache+size"></a>
+
+## lruCache.size() ⇒ <code>number</code>
+**Returns**: <code>number</code> - the current size of the cache  
+
+<br><a name="LruCache+capacity"></a>
+
+## lruCache.capacity() ⇒ <code>number</code>
+**Returns**: <code>number</code> - the current maximum buffer capacity of the cache  
--- __tests__/lruCache.test.js
+++ __tests__/lruCache.test.js
@@ -3,7 +3,7 @@ import test from 'tape';
 import LruCache from '../lib';
 
 const subject = 'the lru cache';
-const init = cap => new LruCache(cap);
+const init = (cap, cb) => new LruCache(cap, cb);
 
 test(`${subject} pops off entries if it meets or exceeds the capacity`, t => {
 	const cap = 6;
@@ -18,8 +18,9 @@ test(`${subject} pops off entries if it meets or exceeds the capacity`, t => {
 	lru.put('END', 6);
 	lru.put('END', 6);
 
-	t.equals(lru.adapter.size(), cap);
+	t.equals(lru.size(), cap);
 	t.equals(lru.adapter.head().value.key, 'END');
+	t.equals(lru.has('END'), true);
 	t.equals(lru.cache.size, cap);
 	t.equals(lru.get('key1'), null);
 	t.end();
@@ -55,10 +56,10 @@ test(`${subject} returns null when retrieving a non-extant value`, t => {
 	t.end();
 });
 
-test(`${subject} returns null when deleting a non-extant value`, t => {
+test(`${subject} returns false when deleting a non-extant value`, t => {
 	const lru = init();
 
-	t.equals(lru.del('enoexist'), null);
+	t.equals(lru.del('enoexist'), false);
 	t.end();
 });
 
@@ -83,7 +84,7 @@ test(`${subject} size remains consistent with that of the internal list`, t => {
 	lru.get('key9');
 	lru.del('key7');
 
-	t.equals(lru.cache.size, lru.adapter.size());
+	t.equals(lru.cache.size, lru.size());
 	t.end();
 });
 
@@ -98,7 +99,7 @@ test(`${subject} size remains consistent with that of the internal list`, t => {
 
 	lru.get('key49');
 
-	t.equals(lru.cache.size, lru.adapter.size());
+	t.equals(lru.cache.size, lru.size());
 	t.end();
 });
 
@@ -114,22 +115,22 @@ test(`${subject} internal list maintains integrity when compared to manually obs
 	lru.put('key2'); // head = k2, tail = k1
 	t.equals(lru.adapter.head().value.key, 'key2');
 	t.equals(lru.adapter.tail().value.key, 'key1');
-	t.equals(lru.adapter.size(), 2);
+	t.equals(lru.size(), 2);
 
 	lru.get('key1'); // head = k1, tail = k2
 	t.equals(lru.adapter.head().value.key, 'key1');
 	t.equals(lru.adapter.tail().value.key, 'key2');
-	t.equals(lru.adapter.size(), 2);
+	t.equals(lru.size(), 2);
 
 	lru.put('key3'); // head = k3, tail = k2  | k3, k1, k2 |
 	t.equals(lru.adapter.head().value.key, 'key3');
 	t.equals(lru.adapter.tail().value.key, 'key2');
-	t.equals(lru.adapter.size(), 3);
+	t.equals(lru.size(), 3);
 
 	lru.get('key1'); // | k1, k3, k2 |
 	t.equals(lru.adapter.head().value.key, 'key1');
 	t.equals(lru.adapter.tail().value.key, 'key2');
-	t.equals(lru.adapter.size(), 3);
+	t.equals(lru.size(), 3);
 
 	lru.put('key4'); // head = k4, tail = k1 -> | k4, k1, k3 |
 	t.equals(lru.adapter.head().value.key, 'key4');
@@ -138,8 +139,217 @@ test(`${subject} internal list maintains integrity when compared to manually obs
 	lru.get('key1'); // head = k1, tail = k3 -> | k1, k4, k3 |
 	t.equals(lru.adapter.head().value.key, 'key1');
 	t.equals(lru.adapter.tail().value.key, 'key3');
-	t.equals(lru.adapter.size(), 3);
+	t.equals(lru.size(), 3);
 
-	t.equals(lru.cache.size, lru.adapter.size());
+	t.equals(lru.cache.size, lru.size());
+	t.end();
+});
+
+test(`${subject} should return keys`, t => {
+	const cap = 15;
+
+	const lru = init(cap);
+
+	for (let i = 0; i <= 20; i++) {
+		lru.put(`key${i}`, i);
+	}
+
+	const keys = lru.keys();
+
+	for (let i = 0; i < 15; i++) {
+		t.equals(keys[i], `key${i + 6}`);
+	}
+
+	t.end();
+});
+
+test(`${subject} should maintain knowledge of the lru, and support dropping`, t => {
+	const maxcap = 3;
+	let evictions = 0;
+
+	const lru = init(maxcap, function incr(k, v) {
+		if (k != v) t.fail(`Evicted instances not synced; Have (k=${k},v=${v}), Want (k=v)`);
+		evictions++;
+	});
+
+	lru.put(1, 1);
+	lru.put(2, 2);
+	lru.get(1);
+	lru.put(3, 3);
+	lru.get(1);
+	lru.put(4, 4);
+	lru.get(1);
+	lru.put(5, 5);
+
+	if (lru.size() != maxcap) {
+		t.fail(`Eviction policy failure; Have size ${lru.size()}, Want size ${maxcap}`);
+	}
+
+	if (lru.lru().join() != '4,4') {
+		t.fail(`Least recently used failure; Have ${lru.lru()} Want ${[4,4]}`);
+	}
+
+	if (evictions != 2)  {
+		t.fail(`Eviction policy failure; Have ${evictions} evictions, Want ${2} evictions`);
+	}
+
+	evictions = 0;
+	lru.drop();
+
+	if (evictions != maxcap) {
+		t.fail(`Expected drop to remove all cached values; Have ${evictions} evictions, Want ${maxcap} evictions`);
+	}
+
+	if (lru.keys().length != 0) {
+		t.fail(`Expected drop to remove all keys; Have ${lru.keys().length} keys, Want ${0} keys`);
+	}
+
+	t.end();
+});
+
+test(`${subject} capably deletes items as expected`, t => {
+	const maxcap = 9;
+	let evictions = 0;
+
+	const lru = init(maxcap, function incr(k, v) {
+		if (k != v) t.fail(`Evicted instances not synced; Have (k=${k},v=${v}), Want (k=v)`);
+		evictions++;
+	});
+
+	for (let i = 0; i <= maxcap; i++) lru.put(i, i);
+
+	if (lru.size() != maxcap) t.fail(`Size mismatch; Have ${lru.size()}, Want ${maxcap}`);
+
+	let c = 0;
+
+	function r (k) {
+		c++;
+		if (!lru.del(k)) t.fail(`Failed to delete item ${k}`);
+
+		if (lru.size() != maxcap - c) t.fail(`Size mismatch; Have ${lru.size()} Want ${c}`)
+
+		if (lru.has(k)) t.fail(`Failed to delete key ${k}`)
+	}
+
+	r(maxcap);
+	r(maxcap - 1);
+	r(maxcap - 3);
+
+	t.end();
+});
+
+test(`${subject} method 'has' is inconsequential qua eviction policies`, t => {
+	const maxcap = 9;
+	let evictions = 0;
+
+	const lru = init(maxcap, function incr(k, v) {
+		if (k != v) t.fail(`Evicted instances not synced; Have (k=${k},v=${v}), Want (k=v)`);
+		evictions++;
+	});
+
+
+	for (let i = maxcap + 1; i < maxcap * 2; i++) {
+		lru.put(i, i);
+		lru.has(i);
+	}
+
+	if (evictions != 0) {
+		t.fail(`Has should not trigger the eviction policy; Have ${evictions} evictions, Want ${0} evictions`);
+	}
+
+	t.end();
+});
+
+test(`${subject} should resize as expected`, t => {
+	const maxcap = 9;
+	let evictions = 0;
+
+	const lru = init(maxcap, function incr(k, v) {
+		if (k != v) t.fail(`Evicted instances not synced; Have (k=${k},v=${v}), Want (k=v)`);
+		evictions++;
+	});
+
+	for (let i = 0; i < maxcap; i++) lru.put(i, i);
+
+	lru.resize(maxcap / 3);
+
+	if (evictions != 6) {
+		t.fail(`Eviction policy failed; Have ${evictions} evictions, Want ${6} evictions`);
+	}
+
+	lru.resize(lru.size() + maxcap)
+
+	for (let i = 0; i <= lru.capacity(); i++) lru.put(i, i);
+
+	if (evictions != 6 + 1) {
+		t.fail(`Eviction policy failed; Have ${evictions} evictions, Want ${6 + 1} evictions`);
+	}
+
+	t.end();
+});
+
+test(`${subject}, mitigation and default values`, t => {
+	const maxcap = 9;
+	let evictions = 0;
+
+	const lru = init(maxcap, function incr(k, v) {
+		if (k != v) t.fail(`Evicted instances not synced; Have (k=${k},v=${v}), Want (k=v)`);
+		evictions++;
+	});
+
+	if (Array.isArray(lru.lru())) t.fail('LRU should be nil');
+
+	if (lru.del(9)) t.fail('Deleting a non-extant value should return false');
+
+	if (lru.has(9)) t.fail('Has used with a non-extant key should return false');
+
+	for (let i = 0; i < lru.capacity(); i++) lru.put(i, i);
+
+	t.equals(evictions, 0);
+ 	t.equals(lru.size(), maxcap);
+
+	t.end();
+});
+
+
+test(`${subject} eviction policy should maintain integrity across many transactions`, t => {
+	const maxcap = 256;
+	let evictions = 0;
+
+	const lru = init(maxcap, function incr(k, v) {
+		if (k != v) t.fail(`Evicted instances not synced; Have (k=${k},v=${v}), Want (k=v)`);
+		evictions++;
+	});
+
+
+	for (let i = 0; i < maxcap * 2; i++) lru.put(i, i);
+
+
+	if (lru.size() != maxcap) {
+		t.fail(`Cache capacity failure; Have %v, Want %v`, lru.size(), maxcap);
+	}
+
+	if (evictions != maxcap) {
+		t.fail(`Cache eviction failure; Have %v, Want %v`, evictions, maxcap);
+	}
+
+	for (const [i, k] of lru.keys().entries()) {
+		const v = lru.get(k);
+
+		if (!v) t.fail('Key retrieval failure');
+
+		if (v != k) t.fail(`Invalid key; Have ${v}, Want ${k}`);
+
+		if (v != i + maxcap) t.fail(`Invalid key; Have ${v}, Want ${i + maxcap}`);
+	}
+
+	for (let i = 0; i < maxcap; i++) {
+		if (lru.has(i)) t.fail(`Cache contains stale value; ${i} should have been evicted`);
+	}
+
+	for (let i = maxcap; i < maxcap * 2; i++) {
+		if (!lru.get(i)) t.fail(`Premature cache eviction; ${i} should not have been evicted`);
+	}
+
 	t.end();
 });
